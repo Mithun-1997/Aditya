d458371b00b7c2673e8539b5f9024642dd676eef
mithun <devopsdocter@gmail.com>
2025-02-23 15:15:16 +0530

    api first flow

--- devops/src/main/java/project/Validation.java
+++
@@ -1,7 +0,0 @@
-package project;
-
-public class Validation {
-    public static boolean isValidMobileNumber(String mobileNumber) {
-        return mobileNumber != null && mobileNumber.matches("\\d{10}");
-    }
-}
